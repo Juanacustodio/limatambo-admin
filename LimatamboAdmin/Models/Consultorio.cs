a1ab1766a654d7534fe979247a6440a29290f077
Juana Custodio <custodim.juana@gmail.com>
2020-01-18 00:01:25 -0500

    add cnosultorio crud

--- LimatamboAdmin/Models/Consultorio.cs
+++ LimatamboAdmin/Models/Consultorio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,15 @@ namespace LimatamboAdmin.Models
 {
     public class Consultorio
     {
+        static public string titulo = "Consultorio";
+        static public string tituloPlural = "Consultorios";
+
         public int ID { get; set; }
         public string nombre { get; set; }
         public int piso { get; set; }
         public int idEspecialidadPorSede { get; set; }
+
+        [ForeignKey("idEspecialidadPorSede")]
         public virtual EspecialidadPorSede especialidadPorSede { get; set; }
     }
 }
